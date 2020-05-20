c7acb62a4d023e1eda67db65944bee6d4dfe3019
bachandi <62039342+bachandi@users.noreply.github.com>
2020-05-20 02:11:21 +0200

    Disable automatic timer as we do it by ourself

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ RaspberryPiInfo.prototype.setUpServices = function () {
 		.setCharacteristic(Characteristic.SerialNumber, os.hostname()) // Note that if your Eve.app is controlling more than one accessory for each type, the serial number should be unique, otherwise Eve.app will merge the histories.
 		.setCharacteristic(Characteristic.FirmwareRevision, packageFile.version);
 	
-	this.fakeGatoHistoryService = new FakeGatoHistoryService("weather", this, { storage: 'fs' });
+  this.fakeGatoHistoryService = new FakeGatoHistoryService("weather", this, { storage: 'fs', disableTimer: true });
 	
 	let uuid1 = UUIDGen.generate(that.name + '-Uptime');
 	info = function (displayName, subtype) {
